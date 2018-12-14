9e73db5b1b24e5b0e9719b8436360bd3225c3744
rubtavpic <rubentavero@outlook.com>
2018-12-14 09:33:39 +0100

    Primer ejemplo hola mundo. seccion_2

---
+++ HolaMundo/src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args){
+        //este es un comentario
+        /*
+        este es un comentario
+        de multiples lineas
+         */
+        System.out.println("Hola Mundo");
+        System.out.println("Adios");
+    }
+}
