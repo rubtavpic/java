b8df8f2f28180e61f5886fd8ba76b69a08340b92
Ruben Tavero <rubentavero@outlook.com>
2018-12-28 22:05:55 +0100

    Terminada seccion 2 (portatil)

---
+++ HolaMundo/src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        //Esto es un comentario
+        /*
+        Este es un comentario
+        de multiples lineas
+         */
+        System.out.println("Hola mundo");
+        System.out.println("Adios");
+    }
+}
